f69f84c3824eb1d9923bb92101ff1ea076e8ea48
usercode <2958488+usercode@users.noreply.github.com>
2024-06-01 20:03:17 +0200

    Renamed ReadResultFromJsonAsync

--- src/SmartResults/Http/HttpResponseMessageExtensions.cs
+++ src/SmartResults/Http/HttpResponseMessageExtensions.cs
@@ -9,24 +9,24 @@ namespace SmartResults;
 
 public static class HttpResponseMessageExtensions
 {
-    public static async Task<Result> ToResultAsync(this Task<HttpResponseMessage> responseTask, JsonSerializerOptions? jsonSerializerOptions = null)
+    public static async Task<Result> ReadResultFromJsonAsync(this Task<HttpResponseMessage> responseTask, JsonSerializerOptions? jsonSerializerOptions = null)
     {
-        return await (await responseTask).ToResultAsync(jsonSerializerOptions);
+        return await (await responseTask).ReadResultFromJsonAsync(jsonSerializerOptions);
     }
 
-    public static async Task<Result> ToResultAsync(this HttpResponseMessage response, JsonSerializerOptions? jsonSerializerOptions = null)
+    public static async Task<Result> ReadResultFromJsonAsync(this HttpResponseMessage response, JsonSerializerOptions? jsonSerializerOptions = null)
     {
         Result result = await response.Content.ReadFromJsonAsync<Result>(jsonSerializerOptions);
 
         return result;
     }
 
-    public static async Task<Result<T>> ToResultAsync<T>(this Task<HttpResponseMessage> responseTask, JsonSerializerOptions? jsonSerializerOptions = null)
+    public static async Task<Result<T>> ReadResultFromJsonAsync<T>(this Task<HttpResponseMessage> responseTask, JsonSerializerOptions? jsonSerializerOptions = null)
     {
-        return await (await responseTask).ToResultAsync<T>(jsonSerializerOptions);
+        return await (await responseTask).ReadResultFromJsonAsync<T>(jsonSerializerOptions);
     }
 
-    public static async Task<Result<T>> ToResultAsync<T>(this HttpResponseMessage response, JsonSerializerOptions? jsonSerializerOptions = null)
+    public static async Task<Result<T>> ReadResultFromJsonAsync<T>(this HttpResponseMessage response, JsonSerializerOptions? jsonSerializerOptions = null)
     {
         Result<T> result = await response.Content.ReadFromJsonAsync<Result<T>>(jsonSerializerOptions);
 
